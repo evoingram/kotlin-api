553f4b82e24d9a9d346402d29a61cb9e574b50c6
Erica L. Ingram <inquiries@aquoco.co>
2023-05-20 18:15:30 -0700

    KA-1: working API with h2 database

---
+++ src/main/kotlin/com/example/kotlinapipg1/Kotlinapipg1Application.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinapipg1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kotlinapipg1Application
+
+fun main(args: Array<String>) {
+    runApplication<Kotlinapipg1Application>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinapipg1/Kotlinapipg1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinapipg1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kotlinapipg1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
