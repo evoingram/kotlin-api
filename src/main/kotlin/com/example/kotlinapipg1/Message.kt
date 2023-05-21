6da74fbe2e21d0c4b5d0ee9993055d5b7b447ed9
Erica L. Ingram <inquiries@aquoco.co>
2023-05-20 18:41:11 -0700

    KA-1: reorganize files

--- src/main/kotlin/com/example/kotlinapipg1/Message.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlinapipg1
-
-import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Table
-
-@Table("MESSAGES")
-data class Message(@Id var id: String?, val text: String)
--- src/main/kotlin/com/example/kotlinapipg1/MessageRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotlinapipg1
-
-import org.springframework.data.repository.CrudRepository
-
-interface MessageRepository : CrudRepository<Message, String>
